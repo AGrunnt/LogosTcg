592cc301f9e17b4d369724a28205eeb67e9fade8
AGrunnt <trenton.metcalfe@eastern.edu>
2025-07-07 12:33:00 -0400

    starting multi board work

--- Assets/Scripts/OfflineLobbyManager.cs
+++ Assets/Scripts/OfflineLobbyManager.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -5,8 +6,12 @@ namespace LogosTcg
 {
     public class OfflineLobbyManager : MonoBehaviour
     {
+        public TextMeshProUGUI roundNumUi;
+
         public void LoadPlayBoard()
         {
+            StaticData.roundNums = int.Parse(roundNumUi.text);
+
             SceneManager.LoadSceneAsync("PlayBoard", LoadSceneMode.Single);
         }
     }
