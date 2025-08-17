4bef2ed9f1f6250e28f992f6505d6d400a27979f
AGrunnt <trenton.metcalfe@eastern.edu>
2025-08-17 09:04:23 -0500

    coin

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -1,10 +1,18 @@
+using TMPro;
 using UnityEngine;
 
 namespace LogosTcg
 {
     public class Coin : MonoBehaviour
     {
-        public int value;
+        public int value = 5;
+        [SerializeField] TextMeshProUGUI valueTmp;
+
+        public void SetValueTmp(int val)
+        {
+            value = val;
+            valueTmp.text = value.ToString();
+        }
 
         private void Start()
         {
--- Assets/Scripts/CoinStack.cs
+++ Assets/Scripts/CoinStack.cs
@@ -7,33 +7,89 @@ namespace LogosTcg
     public class CoinStack : MonoBehaviour
     {
         [SerializeField] private GameObject coinPrefab;
-        [SerializeField] private int value;
+        //[SerializeField] private int value = 1;
+        [SerializeField] private bool visible = false;
+        [SerializeField] private Transform coinSlot1;
+        [SerializeField] private Transform coinSlot5;
+        [SerializeField] private Transform coinSlot10;
 
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         void Start()
         {
             //Need to pause for layout groups to have a second to adjust
+            //StartCoroutine(SetUpCoinStacks());
+        }
+
+        [ContextMenu("Run/Visible")]
+        public void ToggleVisible()
+        {
+            if(visible)
+            {
+                visible = false;
+                coinSlot1.gameObject.SetActive(false);
+                coinSlot5.gameObject.SetActive(false);
+                coinSlot10.gameObject.SetActive(false);
+            }
+            else
+            {
+                visible = true;
+                coinSlot1.gameObject.SetActive(true);
+                coinSlot5.gameObject.SetActive(true);
+                coinSlot10.gameObject.SetActive(true);
+            }
+        }
+
+
+        [ContextMenu("Run/Draw One Card")]
+        public void RunSetUp()
+        {
             StartCoroutine(SetUpCoinStacks());
         }
 
-        private IEnumerator SetUpCoinStacks()
+
+        
+        public IEnumerator SetUpCoinStacks()
         {
             yield return new WaitForSeconds(0.5f);
 
-            var coin1 = Instantiate(coinPrefab, transform, false);
-            var coin2 = Instantiate(coinPrefab, transform, false);
-            coin1.GetComponent<Coin>().value = value;
-            coin2.GetComponent<Coin>().value = value;
+            var coin1 = Instantiate(coinPrefab, coinSlot1, false);
+            var coin2 = Instantiate(coinPrefab, coinSlot1, false);
+            coin1.GetComponent<Coin>().SetValueTmp(1);
+            coin2.GetComponent<Coin>().SetValueTmp(1);
+
+            var coin3 = Instantiate(coinPrefab, coinSlot5, false);
+            var coin4 = Instantiate(coinPrefab, coinSlot5, false);
+            coin3.GetComponent<Coin>().SetValueTmp(5);
+            coin4.GetComponent<Coin>().SetValueTmp(5);
+
+            var coin5 = Instantiate(coinPrefab, coinSlot10, false);
+            var coin6 = Instantiate(coinPrefab, coinSlot10, false);
+            coin5.GetComponent<Coin>().SetValueTmp(10);
+            coin6.GetComponent<Coin>().SetValueTmp(10);
         }
 
-        private void OnTransformChildrenChanged()
+        void OnTransformChildrenChanged()
         {
-            if (transform.childCount < 3)
+            /*
+            if (transform.childCount < 2 && visible)
             {
                 var coin = Instantiate(coinPrefab, transform, false);
                 coin.GetComponent<Coin>().value = value;
                 coin.transform.localPosition = Vector3.zero;
             }
+            if (transform.childCount < 2 && visible)
+            {
+                var coin = Instantiate(coinPrefab, transform, false);
+                coin.GetComponent<Coin>().value = value;
+                coin.transform.localPosition = Vector3.zero;
+            }
+            if (transform.childCount < 2 && visible)
+            {
+                var coin = Instantiate(coinPrefab, transform, false);
+                coin.GetComponent<Coin>().value = value;
+                coin.transform.localPosition = Vector3.zero;
+            }
+            */
         }
     }
 }
